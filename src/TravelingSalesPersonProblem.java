4910f586416d4564eca8ac95ec4e16f6696b2b91
skia <sairagav2003@gmail.com>
2023-08-25 10:22:48 +0530

    Revert "Revert "TSP rewrite""
    This reverts commit 7abd8c70400d10461a5622b6830538378aed7d3c.

--- src/TravelingSalesPersonProblem.java
+++ src/TravelingSalesPersonProblem.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 import java.util.LinkedList;
 
 class TGraph {
-    int costMatrix[][];
+    int c[][];
     int n;
     // boolean visited[];
 
     TGraph(int n) {
         this.n = n;
-        costMatrix = new int[n][n];
+        c = new int[n][n];
         // visited = new boolean[n];
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter cost matrix:");
@@ -16,9 +16,9 @@ class TGraph {
             for (int u = 0; u < n; u++) {
                 int edgeWeight = sc.nextInt();
                 if (edgeWeight == 0)
-                    costMatrix[v][u] = 99999;
+                    c[v][u] = 99999;
                 else
-                    costMatrix[v][u] = edgeWeight;
+                    c[v][u] = edgeWeight;
             }
         }
     }
@@ -27,11 +27,11 @@ class TGraph {
 public class TravelingSalesPersonProblem {
 
     static int ogSource;
-    static LinkedList<Integer> visitSequence = new LinkedList<>();
-
-    // graphSoln is useless as you are never really tabulating the results
 
     static int travelingSalesPerson(TGraph g, int source, LinkedList<Integer> subset) {
+        if (subset.isEmpty()) {
+            return g.c[source][ogSource];
+        }
         int intermediateCosts[] = new int[g.n];
         for (int vertex = 0; vertex < g.n; vertex++)
             intermediateCosts[vertex] = 9999;
@@ -39,27 +39,19 @@ public class TravelingSalesPersonProblem {
         for (int vertex : subset) {
             LinkedList<Integer> newSubset = new LinkedList<>(subset);
             newSubset.removeFirstOccurrence(vertex);
-            System.out.println("newSubset:" + newSubset); // debug line
-            if (newSubset.isEmpty()) {
-                intermediateCosts[vertex] = g.costMatrix[vertex][ogSource];
-            }
-            for (int subVerticies : newSubset)
-                intermediateCosts[subVerticies] = travelingSalesPerson(g, vertex, newSubset);
+            intermediateCosts[vertex] = g.c[source][vertex] + travelingSalesPerson(g, vertex, newSubset);
         }
 
         int tempweight = 9999;
         int minPathVertex = -1;
         for (int v = 0; v < g.n; v++) {
-            if ((g.costMatrix[v][source] + intermediateCosts[v]) < tempweight) {
-                tempweight = g.costMatrix[v][source] + intermediateCosts[v];
+            if (intermediateCosts[v] < tempweight) {
+                tempweight = intermediateCosts[v];
                 minPathVertex = v;
             }
         }
-
-        // System.out.printf("\n\n\tDEBUG\tsource: %d, minPathVertex: %d ", source,
-        // minPathVertex);
-        // visitSequence.push(minPathVertex + 1); i dunno when to push
-        return g.costMatrix[source][minPathVertex] + intermediateCosts[minPathVertex];
+        // I still havent figured out how to print visit sequence
+        return intermediateCosts[minPathVertex];
     }
 
     public static void main(String[] args) {
@@ -74,20 +66,14 @@ public class TravelingSalesPersonProblem {
             if (vertex != ogSource)
                 subset.add(vertex);
         }
-        visitSequence.push(ogSource + 1);
+        
         int totalCost = travelingSalesPerson(g, ogSource, subset);
-        visitSequence.push(ogSource + 1);
 
-        System.out.println("\n\nVisiting sequence:");
-        System.out.println(visitSequence);
         System.out.println("Total tour cost: " + totalCost);
         s.close();
     }
 }
 /*
  * sample input
-  0 10 15 20
-  5 0 9 10
-  6 13 0 12
-  8 8 9 0
+ * 0 10 15 20 5 0 9 10 6 13 0 12 8 8 9 0
  */
